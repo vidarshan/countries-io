0103994dfe5b92ca71c5a11580e28ce479427ddf
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-07-19 20:10:48 +0530

    Implemented search functionality

--- src/reducers/countryReducers.js
+++ src/reducers/countryReducers.js
@@ -5,6 +5,9 @@ import {
   GET_COUNTRY_REQUEST,
   GET_COUNTRY_SUCCESS,
   GET_COUNTRY_FAIL,
+  SEARCH_COUNTRY_REQUEST,
+  SEARCH_COUNTRY_SUCCESS,
+  SEARCH_COUNTRY_FAIL,
 } from './../constants/countryConstants';
 
 export const getAllReducer = (state = { countries: [] }, action) => {
@@ -14,6 +17,19 @@ export const getAllReducer = (state = { countries: [] }, action) => {
     case GET_ALL_COUNTRIES_SUCCESS:
       return { loading: false, countries: action.payload };
     case GET_ALL_COUNTRIES_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const searchCountryReducer = (state = { results: [] }, action) => {
+  switch (action.type) {
+    case SEARCH_COUNTRY_REQUEST:
+      return { loading: true };
+    case SEARCH_COUNTRY_SUCCESS:
+      return { loading: false, results: action.payload };
+    case SEARCH_COUNTRY_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
