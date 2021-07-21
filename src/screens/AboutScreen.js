056539e0524c548cbf61f73e07fec74a2887d35e
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-07-21 22:26:59 +0530

    About screen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const AboutScreen = () => {
-  return <div>About</div>;
+  return (
+    <div className='about-container'>
+      <img className='about-image' src='/images/location.png' alt='' />
+
+      <div className='about-title'>countries.io</div>
+      <div className='about-description'>
+        Made by vidarshan with the use of restcountries.eu API
+      </div>
+      <div className='about-version'>Version 1.0 [July 2021]</div>
+    </div>
+  );
 };
 
 export default AboutScreen;
