3eb4a5edd795765143af84a4b04cbd3c1c60d598
vidarshanadithya98 <vidarshanadithya3@gmail.com>
2021-07-24 11:45:38 +0530

    Adding translation abbreviations

---
+++ src/data/Translations.js
@@ -0,0 +1,12 @@
+export const translationAbbreviations = [
+  { de: 'German' },
+  { fr: 'French' },
+  { it: 'Italian' },
+  { pt: 'Portuguese' },
+  { hr: 'Croatian' },
+  { es: 'Spanish' },
+  { ja: 'Japanese' },
+  { br: 'Spanish' },
+  { nl: 'Dutch' },
+  { fa: 'Persian' },
+];
